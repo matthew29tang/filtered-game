ba65ce2eadcd79b3194936504b976944c99f6b0d
Matthew Tang <matthewtang@berkeley.edu>
2024-09-03 22:14:22 -0700

    Updates

---
+++ precache-manifest.470c2d3002e9db0bffd9e9d0bbb76868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2728936a1e5190987c59a036683cd8f9",
+    "url": "/filtered-game/index.html"
+  },
+  {
+    "revision": "e7f4504b3bfcaaad84d5",
+    "url": "/filtered-game/static/css/main.b50f9b93.chunk.css"
+  },
+  {
+    "revision": "200eee4d102938b84a1c",
+    "url": "/filtered-game/static/js/2.913efa36.chunk.js"
+  },
+  {
+    "revision": "e7f4504b3bfcaaad84d5",
+    "url": "/filtered-game/static/js/main.ab8d4aa3.chunk.js"
+  },
+  {
+    "revision": "183daf961aa240b0d44d",
+    "url": "/filtered-game/static/js/runtime~main.ac668a13.js"
+  }
+]);
