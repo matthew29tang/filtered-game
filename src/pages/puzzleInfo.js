c972cf6617ec7341c8f4bb263695ae7b054431ad
Matthew Tang <matthewtang@berkeley.edu>
2024-09-04 23:01:11 -0700

    Fix bug for us states puzzle

--- src/pages/puzzleInfo.js
+++ src/pages/puzzleInfo.js
@@ -15,7 +15,7 @@ const rrWords = (msg) => {
 
 const states = (msg) => {
   const words = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'];
-  return words.includes(msg.toLowerCase());
+  return words.includes(msg.toUpperCase());
 }
 
 function isSubSequence(s1, s2, m, n)
